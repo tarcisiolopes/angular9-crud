6fe46f006d8f0a218c4fe0e12de5c8f0d74b7911
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-07-29 23:57:22 -0300

    Componente Criar Produto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -11,6 +12,9 @@ const routes: Routes = [
   },{
     path: "products",
     component: ProductCrudComponent
+  },{
+    path: "products/create",
+    component: ProductCreateComponent
   }
 ];
 
