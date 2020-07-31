2b15881609eb42140e1abf140ed38f2f5b2cba88
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-07-30 22:10:55 -0300

    Produto Service

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -7,15 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  // atributoLegal = "qualquer"
-
-  constructor() { }
+  constructor(private productService: ProductService, 
+      private router: Router) { }
 
   ngOnInit(): void {
   }
-  
-  // fazerAlgo(): void {
-  //   console.log('Fazendo algo!')
-  // }
 
+  createProduct(): void {
+    this.productService.showMensage('Operação executada com sucesso!')
+  }
+  cancelar(): void {
+    this.router.navigate(['/products'])
+  }
 }
