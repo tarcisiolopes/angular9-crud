060aa1e476c6c63214bc6c9361e7712ad3c14ea6
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-07-30 22:42:23 -0300

    Formulário Criar Produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,8 +18,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-
 import { HttpClientModule }from '@angular/common/http'
+
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule }from '@angular/material/form-field'
+import { MatInputModule }from '@angular/material/input'
 
 @NgModule({
   declarations: [
@@ -41,7 +44,12 @@ import { HttpClientModule }from '@angular/common/http'
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
