dd7d6b4f791697cc2e7d5e7e2c922319b533e6ab
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-07-30 22:29:32 -0300

    Método Criar Produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -9,6 +10,11 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: null
+  }
+
   constructor(private productService: ProductService, 
       private router: Router) { }
 
@@ -16,7 +22,11 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMensage('Operação executada com sucesso!')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMensage('Operação executada com sucesso!')
+      this.router.navigate(['/products'])
+    })
+    
   }
   cancelar(): void {
     this.router.navigate(['/products'])
