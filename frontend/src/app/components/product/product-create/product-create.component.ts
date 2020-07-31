abea1358ca52f08733d6fe67543d2d37390c830d
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-07-30 21:45:19 -0300

    Navegação para a tela de criar produtos

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  // atributoLegal = "qualquer"
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  // fazerAlgo(): void {
+  //   console.log('Fazendo algo!')
+  // }
 
 }
