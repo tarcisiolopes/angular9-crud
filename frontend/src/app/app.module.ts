8e2f6037d50a5d1f3d9851b6ad4f3b575aa714fa
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-07-31 10:34:22 -0300

    Tabela Produtos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +23,15 @@ import { HttpClientModule }from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule }from '@angular/material/form-field'
 import { MatInputModule }from '@angular/material/input';
-import { ProductReadComponent } from './components/product/product-read/product-read.component'
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
@@ -34,7 +42,7 @@ import { ProductReadComponent } from './components/product/product-read/product-
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    ProductReadComponent
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,11 +57,18 @@ import { ProductReadComponent } from './components/product/product-read/product-
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
 
 
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-br'
+  },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
